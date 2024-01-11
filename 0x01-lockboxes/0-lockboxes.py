4507769ebedd30bd22cac5630ab31085c23c209c
DINAMOW <meemoo102039@gmail.com>
2024-01-11 17:11:07 +0200

    fix the code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,7 +9,6 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, else False
     """
-    empty_boxes = 0
 
     if len(boxes) == 0:
         return False
@@ -18,16 +17,22 @@ def canUnlockAll(boxes):
         return True
 
     result = []
-
+    result.append(0)
+    req_indexs = []
     for i in boxes[0]:
         result.append(i)
+    for i in boxes:
+        if len(i) != 0:
+            req_indexs.append(boxes.index(i))
     for i in result:
+        if i >= len(boxes):
+            del result[result.index(i)]
+            continue
         if len(boxes[i]) > 0:
             for j in boxes[i]:
                 if j not in result:
                     result.append(j)
-        else:
-            empty_boxes += 1
-    if len(result) == len(boxes) - empty_boxes:
-        return True
-    return False
+    for i in req_indexs:
+        if i not in result:
+            return False
+    return True
