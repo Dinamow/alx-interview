5d0b40d27af437effbf8d7109e06aedd63d58324
DINAMOW <meemoo102039@gmail.com>
2024-01-11 17:19:40 +0200

    better code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
-"""canUnlockAll module"""
+"""canUnlockAll function"""
 
 
 def canUnlockAll(boxes):
-    """canUnlockAll function
-    Args:
-        boxes (List[List[int]]): list of lists
-    Returns:
-        bool: True if all boxes can be opened, else False
-    """
-
-    if len(boxes) == 0:
-        return False
-
-    if len(boxes) == 1:
-        return True
-
-    result = []
-    result.append(0)
-    req_indexs = []
-    for i in boxes[0]:
-        result.append(i)
-    for i in boxes:
-        if len(i) != 0:
-            req_indexs.append(boxes.index(i))
-    for i in result:
-        if i >= len(boxes):
-            del result[result.index(i)]
-            continue
-        if len(boxes[i]) > 0:
-            for j in boxes[i]:
-                if j not in result:
-                    result.append(j)
-    for i in req_indexs:
-        if i not in result:
-            return False
-    return True
+    """see if all boxes can be opened or not"""
+    keys = {0}
+    stack = [boxes[0]]
+    while stack:
+        pop = stack.pop()
+        for index in pop:
+            if index in keys or index >= len(boxes):
+                continue
+            stack.append(boxes[index])
+            keys.add(index)
+    return len(keys) == len(boxes)
