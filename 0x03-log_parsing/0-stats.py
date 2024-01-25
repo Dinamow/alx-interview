c01e2748fcf167d602f21e910a78e59b98987033
DINAMOW <meemoo102039@gmail.com>
2024-01-25 10:04:23 +0200

    handle some errors

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,19 +3,39 @@
 import sys
 
 
-while True:
-    file_size = 0
-    status_codes = {
-        '200': 0, '301': 0,
-        '400': 0, '401': 0,
-        '403': 0, '404': 0,
-        '405': 0, '500': 0
-    }
-    for i in range(10):
-        raw = sys.stdin.readline().split()
-        file_size += int(raw[-1])
-        status_codes[str(raw[-2])] += 1
+def print_msg(status_codes, file_size):
+    """print the message"""
     print('File size: {:d}'.format(file_size))
-    for i in status_codes.keys():
-        if status_codes[i] != 0:
-            print('{:s}: {:d}'.format(i, status_codes[i]))
+    for key, val in status_codes.items():
+        if val != 0:
+            print('{:s}: {:d}'.format(key, val))
+
+file_size = 0
+status_codes = {
+    '200': 0, '301': 0,
+    '400': 0, '401': 0,
+    '403': 0, '404': 0,
+    '405': 0, '500': 0
+}
+count = 0
+for line in sys.stdin:
+    try:
+        try:
+            raw = line.split()
+            file_size += int(raw[-1])
+            status_codes[str(raw[-2])] += 1
+            count += 1
+            if count / 10 == 1:
+                count = 0
+                print_msg(status_codes, file_size)
+                file_size = 0
+                status_codes = {
+                '200': 0, '301': 0,
+                '400': 0, '401': 0,
+                '403': 0, '404': 0,
+                '405': 0, '500': 0
+                }
+        except BaseException:
+            pass
+    except KeyboardInterrupt:
+        print_msg(status_codes, file_size)
