a1dba0b6dd0bec9adee6c5ed8dcf7948efed96a9
Dinamow <meemoo102039@gmail.com>
2024-01-29 09:43:12 +0200

    first time

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this is UTF-8 validator module"""
+
+
+def validUTF8(data):
+    """this function validates UTF-8 encoding"""
+    for i in data:
+        binary = bin(i)
+        try:
+            binary.to_bytes(1, 'big').decode()
+        except OverflowError:
+            return False
+    return True
