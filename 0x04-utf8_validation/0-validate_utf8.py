1eb38cfeff3d9af74cfcd008f7f80d89eb4331a0
DINAMOW <meemoo102039@gmail.com>
2024-02-01 08:53:36 +0200

    check if its ascii for not

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,9 +5,6 @@
 def validUTF8(data):
     """this function validates UTF-8 encoding"""
     for i in data:
-        binary = bin(i)
-        try:
-            eval(binary).to_bytes(1, 'big').decode()
-        except OverflowError:
+        if i > 255:
             return False
     return True
