78f7a7eb003b9d1d441ce80bfffd90903a480bcd
DINAMOW <meemoo102039@gmail.com>
2024-02-13 18:38:10 +0200

    reduce the lines

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-id = process.argv[2];
-url = 'https://swapi-api.alx-tools.com/api/films/' + id;
-request(url, function (error, response, body) {
-	if (error) {
-		console.error(error);
-	}
-	for (const person of JSON.parse(body).characters) {
-		request(person, function (error, response, body) {
-			if (error) {
-				console.error(error);
-			}
-			console.log(JSON.parse(body).name);
-		});
-	}
+
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  for (const person of JSON.parse(body).characters) {
+    request(person, function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
 });
